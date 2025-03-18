fde1783c68c08cb3e1c52c6817f671d22d16f956
Emily <e.spiers13@gmail.com>
2025-03-18 09:59:43 +0000

    updated collections dev data

--- controllers/collections-controllers.js
+++ controllers/collections-controllers.js
@@ -13,7 +13,7 @@ exports.getCollectionsByUser = (req, res, next) => {
   const { user_id } = req.params;
   fetchCollectionsByUser(user_id)
     .then((collectionsData) => {
-      res.status(200).json(collectionsData);
+      res.status(200).send(collectionsData);
     })
     .catch((err) => {
       next(err);
@@ -24,7 +24,7 @@ exports.getCollectionByUserAndId = (req, res, next) => {
   const { user_id, collection_id } = req.params;
   fetchCollectionByUserAndId(user_id, collection_id)
     .then((collectionData) => {
-      res.status(200).json(collectionData);
+      res.status(200).send(collectionData);
     })
     .catch((err) => {
       next(err);
@@ -40,7 +40,7 @@ exports.postNewCollection = (req, res, next) => {
       if (data) {
         createNewCollection(user_id, collection_name)
           .then((newCollection) => {
-            res.status(201).json(newCollection);
+            res.status(201).send(newCollection);
           })
           .catch((err) => {
             next(err);
@@ -60,7 +60,7 @@ exports.patchCollectionById = (req, res, next) => {
     .then((data) => {
       addObjectToCollectionById(objectId, collection_id, user_id)
         .then((collectionData) => {
-          res.status(200).json(collectionData);
+          res.status(200).send(collectionData);
         })
         .catch((err) => {
           next(err);
@@ -79,7 +79,7 @@ exports.deleteObjectFromCollection = (req, res, next) => {
     .then((data) => {
       removeObjectFromCollectionById(objectId, collection_id, user_id)
         .then((updatedCollection) => {
-          res.status(200).json(updatedCollection);
+          res.status(200).send(updatedCollection);
         })
         .catch((err) => {
           next(err);
@@ -97,7 +97,7 @@ exports.deleteCollectionByUsernamePassword = (req, res, next) => {
     .then((data) => {
       removeCollectionById(user_id, collection_id)
         .then(() => {
-          res.status(204).json({});
+          res.status(204).send({});
         })
         .catch((err) => {
           next(err);
